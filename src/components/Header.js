c84819b087b30b10e67d7f1211724e6098e6ac5f
Giovana Balestrero Sigolo <gigisigolo@gmail.com>
2023-08-07 18:56:20 -0300

    estilizacao nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -65,41 +65,69 @@ const Header = () => {
       animate={isActive ? 'show' : ''} 
       className='bg-pink-200/20 fixed w-full max-w-[1800px] z-50 py-4'>
         
-        <div className='container mx-auto'>
-          <div>
+        <motion.div 
+          variants={staggerContainer(0.3, 1)} 
+          initial='hidden' 
+          animate={'show'} 
+          className='container mx-auto'>
+          <div className='flex justify-between items-center px-4 lg:px-0 relative text-white'>
             {/* botao menu */}
             <motion.div 
-              className={`flex bg-red-400/20 flex-col`} 
+              variants={fadeIn('down', 'tween', 1, 1.4)}
+              className={`${
+                nav ? 'gap-y-0' : 'gap-y-2'
+              } flex flex-col items-center justify-center w-12 h-12 p-12 p-3 order-2 lg:order-none cursor-pointer border-2 rounded-full`} 
               onClick={()=> setNav(!nav)}>
-              {/* barra */}
-              <div>barra1</div>
-              {/* barra */}
-              <div>barra2</div>
+
+              {/* barra 1 */}
+              <motion.div 
+                initial={{
+                  rotate: 0
+                }} 
+                animate={{ 
+                  rotate: nav ? -45 : 0, 
+                  translateY: nav ? 2 : 0 }}
+                className='w-full h-[2px] bg-white'>
+
+              </motion.div>
+              {/* barra 2 */}
+              <motion.div 
+                initial={{
+                  rotate: 0
+                }} 
+                animate={{ 
+                  rotate: nav ? 45 : 0 }}
+                className='w-full h-[2px] bg-white'>
+
+              </motion.div>
             </motion.div>
 
             {/* logo */}
-            <div>
+            <motion.div 
+              variants={fadeIn('down', 'tween', 1.2, 1.4)}>
               <a href='#'>
                 {/* se o header esta ativo, logo 90 x 90 px, caso contrario, 107 x 107 px */}
                 <img className={`${isActive ? 'w-[90px] h-[90px]' : 'w-[107px] h-[107px]'}`} src={LogoWhite} alt=''/>
               </a>
-            </div>
+            </motion.div>
 
             {/* icones  */}
-            <div>
+            <motion.div 
+              variants={fadeIn('down', 'tween', 1.4, 1.4)}
+              className='hidden lg:flex'>
               <Socials/>
-            </div>
+            </motion.div>
 
             {/* nav  */}
             <motion.div 
               variants={navVariants} 
               initial='hidden' 
               animate={nav ? 'show' : ''} 
-              className='absolute bg-accent w-[310px] h-[50vh] right-0 lg:left-0 top-[120px] bottom-0 z-50 rounded-lg shadow-xl'>
+              className='absolute bg-accent w-[310px] h-[60vh] right-0 lg:left-0 top-[120px] bottom-0 z-50 rounded-lg shadow-xl'>
               <Nav/>
             </motion.div>
           </div>
-        </div>
+        </motion.div>
     </motion.header>
   )
 };
