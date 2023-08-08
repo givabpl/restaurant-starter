894121bd8131e91473d69ff06e2fae2b5e043354
Giovana Balestrero Sigolo <gigisigolo@gmail.com>
2023-08-08 16:29:47 -0300

    menu, favicon e titulo do index

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -11,22 +11,55 @@ const Menu = () => {
   const {title, subtitle, modelImg, menuItems, btnText}  = menuData;
   return (
     <section className='min-h-[780px]'>
+
       {/* background */}
       <div className='h-[780px] bg-menu absolute w-full max-w-[1800px] -z-0'></div>
+
       {/* texto */}
       <div className='relative z-10 top-72 lg:top-52'>
-        <div className='container mx-auto flex flex-col items-center text-center'>
-          <h2 className='h2 capitalize text-white max-w-[400px] text-center'>{title}</h2>
-          <p className='text-white/70 cvapitalize mb-8'>{subtitle}</p>
-          <div>
+        <motion.div 
+          variants={staggerContainer}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.1 }}
+          className='container mx-auto flex flex-col items-center text-center'>
+          <motion.h2 
+            variants={fadeIn('down', 'tween', 0.2, 1.6)}
+            className='h2 capitalize text-white max-w-[400px] text-center'>
+            {title}
+          </motion.h2>
+          <motion.p 
+            variants={fadeIn('down', 'tween', 0.4, 1.6)}
+            className='text-white/70 cvapitalize mb-8'>
+            {subtitle}
+          </motion.p>
+          <motion.div
+            variants={fadeIn('down', 'tween', 0.6, 1.6)}>
             <img src={modelImg} alt=''/>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </div>
+
       {/* menu galeria */}
-      <div className='relative top-80 lg:top-96'>
+      <motion.div 
+        initial={{
+          opacity: 0,
+          y: 150,
+        }} 
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          type: 'tween', 
+          delay: 0.2,
+          duration: 1.6,
+          ease: 'easeOut',
+        }}
+        className='relative top-80 lg:top-96'>
         <div className='container mx-auto'>
           <div className='lg:-mt-24 min-h-[590px] z-30 mb-8 md:mb-4 xl:mb-16'>
+
             {/* grid */}
             <div className='grid grid-cols-1 lg:grid-cols-4 min-h-[590px]'>
               {menuItems.map((item, index) => {
@@ -34,27 +67,33 @@ const Menu = () => {
                 return (
                   <div key={index}>
                     <div className='flex flex-row lg:flex-col h-full '>
+
                       {/* imagem */}
                       {/* ordem reversa para a primeira e última imagem */}
                       <div className={`w-[45%] md:w-auto ${index === 1 || index === 3 ? 'lg:order-1' : 'order-none'} `}>
                         <img src={image} alt=''/>
                       </div>
+
                       {/* texto */}
                       <div className='bg-[#fce8d1] flex-1 flex flex-col justify-center px-6 lg:p-12 lg:max-h-[250px] xl:max-h-max'>
-                        <div>
-                          <div>{name}</div>
-                          <div>{price}</div>
+                        <div className='text-center'>
+                          <div className='text-xl font-semibold text-dark xl:text-2xl'>{name}</div>
+                          <div className='my-1 text-[20px] lg:text-[40px] lg:my-6  text-accent font-semibold'>{price}</div>
                           <div>{description}</div>
                         </div>
                       </div>
+
                     </div>
                   </div>
                 );
               })}
             </div>
+
           </div>
+          <button className='btn mx-auto capitalize'>{btnText}</button>
         </div>
-      </div>
+      </motion.div>
+
     </section>
   );
 };
