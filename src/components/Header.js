d0b1944b7e0129490ccb74119bb360a52fafc4c3
Giovana Balestrero Sigolo <gigisigolo@gmail.com>
2023-08-08 15:33:22 -0300

    sobre e ajustes no header e data.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -70,63 +70,63 @@ const Header = () => {
           initial='hidden' 
           animate={'show'} 
           className='container mx-auto'>
-          <div className='flex justify-between items-center px-4 lg:px-0 relative text-white'>
-            {/* botao menu */}
-            <motion.div 
-              variants={fadeIn('down', 'tween', 1, 1.4)}
-              className={`${
-                nav ? 'gap-y-0' : 'gap-y-2'
-              } flex flex-col items-center justify-center w-12 h-12 p-12 p-3 order-2 lg:order-none cursor-pointer border-2 rounded-full`} 
-              onClick={()=> setNav(!nav)}>
-
-              {/* barra 1 */}
+            <div className='flex justify-between items-center px-4 lg:px-0 relative text-white'>
+              {/* botao menu */}
               <motion.div 
-                initial={{
-                  rotate: 0
-                }} 
-                animate={{ 
-                  rotate: nav ? -45 : 0, 
-                  translateY: nav ? 2 : 0 }}
-                className='w-full h-[2px] bg-white'>
+                variants={fadeIn('down', 'tween', 1, 1.4)}
+                className={`${
+                  nav ? 'gap-y-0' : 'gap-y-2'
+                } flex flex-col items-center justify-center w-12 h-12 p-3 order-2 lg:order-none cursor-pointer border-2 rounded-full`} 
+                onClick={()=> setNav(!nav)}>
 
+                {/* barra 1 */}
+                <motion.div 
+                  initial={{
+                    rotate: 0
+                  }} 
+                  animate={{ 
+                    rotate: nav ? -45 : 0, 
+                    translateY: nav ? 2 : 0 }}
+                  className='w-full h-[2px] bg-white'>
+
+                </motion.div>
+                {/* barra 2 */}
+                <motion.div 
+                  initial={{
+                    rotate: 0
+                  }} 
+                  animate={{ 
+                    rotate: nav ? 45 : 0 }}
+                  className='w-full h-[2px] bg-white'>
+
+                </motion.div>
               </motion.div>
-              {/* barra 2 */}
+
+              {/* logo */}
               <motion.div 
-                initial={{
-                  rotate: 0
-                }} 
-                animate={{ 
-                  rotate: nav ? 45 : 0 }}
-                className='w-full h-[2px] bg-white'>
-
+                variants={fadeIn('down', 'tween', 1.2, 1.4)}>
+                <a href='#'className='text'>
+                  {/* se o header esta ativo, logo 90 x 90 px, caso contrario, 107 x 107 px */}
+                  <img className={`${isActive ? 'w-[90px] h-[90px]' : 'w-[107px] h-[107px]'}`} src={LogoWhite} alt=''/>
+                </a>
               </motion.div>
-            </motion.div>
 
-            {/* logo */}
-            <motion.div 
-              variants={fadeIn('down', 'tween', 1.2, 1.4)}>
-              <a href='#'>
-                {/* se o header esta ativo, logo 90 x 90 px, caso contrario, 107 x 107 px */}
-                <img className={`${isActive ? 'w-[90px] h-[90px]' : 'w-[107px] h-[107px]'}`} src={LogoWhite} alt=''/>
-              </a>
-            </motion.div>
+              {/* icones  */}
+              <motion.div 
+                variants={fadeIn('down', 'tween', 1.4, 1.4)}
+                className='hidden lg:flex'>
+                <Socials/>
+              </motion.div>
 
-            {/* icones  */}
-            <motion.div 
-              variants={fadeIn('down', 'tween', 1.4, 1.4)}
-              className='hidden lg:flex'>
-              <Socials/>
-            </motion.div>
-
-            {/* nav  */}
-            <motion.div 
-              variants={navVariants} 
-              initial='hidden' 
-              animate={nav ? 'show' : ''} 
-              className='absolute bg-accent w-[310px] h-[60vh] right-0 lg:left-0 top-[120px] bottom-0 z-50 rounded-lg shadow-xl'>
-              <Nav/>
-            </motion.div>
-          </div>
+              {/* nav  */}
+              <motion.div 
+                variants={navVariants} 
+                initial='hidden' 
+                animate={nav ? 'show' : ''} 
+                className='absolute bg-accent w-[310px] h-[60vh] right-0 lg:left-0 top-[120px] bottom-0 z-50 rounded-lg shadow-xl'>
+                <Nav/>
+              </motion.div>
+            </div>
         </motion.div>
     </motion.header>
   )
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,62 @@
 import React from 'react';
+// dados
+import { menuData } from '../data';
+// motion
+import { motion } from 'framer-motion';
+// variantes
+import { fadeIn, staggerContainer } from '../variants';
 
 const Menu = () => {
-  return <div>Menu</div>;
+  //dados do menu
+  const {title, subtitle, modelImg, menuItems, btnText}  = menuData;
+  return (
+    <section className='min-h-[780px]'>
+      {/* background */}
+      <div className='h-[780px] bg-menu absolute w-full max-w-[1800px] -z-0'></div>
+      {/* texto */}
+      <div className='relative z-10 top-72 lg:top-52'>
+        <div className='container mx-auto flex flex-col items-center text-center'>
+          <h2 className='h2 capitalize text-white max-w-[400px] text-center'>{title}</h2>
+          <p className='text-white/70 cvapitalize mb-8'>{subtitle}</p>
+          <div>
+            <img src={modelImg} alt=''/>
+          </div>
+        </div>
+      </div>
+      {/* menu galeria */}
+      <div className='relative top-80 lg:top-96'>
+        <div className='container mx-auto'>
+          <div className='lg:-mt-24 min-h-[590px] z-30 mb-8 md:mb-4 xl:mb-16'>
+            {/* grid */}
+            <div className='grid grid-cols-1 lg:grid-cols-4 min-h-[590px]'>
+              {menuItems.map((item, index) => {
+                const { image, name, price, description } = item;
+                return (
+                  <div key={index}>
+                    <div className='flex flex-row lg:flex-col h-full '>
+                      {/* imagem */}
+                      {/* ordem reversa para a primeira e última imagem */}
+                      <div className={`w-[45%] md:w-auto ${index === 1 || index === 3 ? 'lg:order-1' : 'order-none'} `}>
+                        <img src={image} alt=''/>
+                      </div>
+                      {/* texto */}
+                      <div className='bg-[#fce8d1] flex-1 flex flex-col justify-center px-6 lg:p-12 lg:max-h-[250px] xl:max-h-max'>
+                        <div>
+                          <div>{name}</div>
+                          <div>{price}</div>
+                          <div>{description}</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Menu;
